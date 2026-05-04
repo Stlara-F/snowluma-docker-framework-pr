59992a6a487d7a49b964782ca71a6541a325087a
DrSmoothl <1787882683@qq.com>
2026-05-04 18:22:12 +0800

    Package latest SnowLuma framework artifact

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@ FRAMEWORK_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 IMAGE="${IMAGE:-snowluma-docker-framework:latest}"
 NAME="${NAME:-snowluma}"
 SNOWLUMA_WEBUI_PORT="${SNOWLUMA_WEBUI_PORT:-8080}"
-SNOWLUMA_WEBUI_HOST_PORT="${SNOWLUMA_WEBUI_HOST_PORT:-${SNOWLUMA_WEBUI_PORT}}"
+SNOWLUMA_WEBUI_HOST_PORT="${SNOWLUMA_WEBUI_HOST_PORT:-5099}"
 
 if ! docker image inspect "${IMAGE}" >/dev/null 2>&1; then
   IMAGE="${IMAGE}" "${SCRIPT_DIR}/build-image.sh"
